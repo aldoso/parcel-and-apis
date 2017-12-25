e0feedfc9e3c34c0c7e9ad9010b4003d8e7ef350
aldoso <alexdobras@gmail.com>
2017-12-25 23:31:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import {app, koko} from './source'
+
+console.log(app.lala)
+document.querySelector('main').innerHTML = koko.la
---
+++ source.js
@@ -0,0 +1,7 @@
+export const app = {
+    lala : 34
+}
+
+export const koko = {
+    la : 1100
+}
