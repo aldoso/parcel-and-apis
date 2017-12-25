5aa914a919ef551f39653bd17db815c7e008897c
aldoso <alexdobras@gmail.com>
2017-12-26 01:13:57 +0200

    final code - doesn't work

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const jokes = require('./source')
+// old way
+// const jokes = require('./source')
+
+// es6 way
+import {jokes} from './source'
+// console.log('it works')
 
 jokes.getOne().then(joke => {
   document.querySelector('main').innerHTML = joke;
--- source.js
+++ source.js
@@ -1,7 +1,19 @@
-module.exports = {
+// old way
+// module.exports = {
+//   getOne: function() {
+//     return new Promise((resolve, reject) => {
+//       fetch('http://api.icndb.com/jokes/random').then(res => res.json()).then(data => {
+//         resolve(data.value.joke)
+//       })
+//     })
+//   }
+// }
+
+//es6 way
+export const jokes = {
   getOne: function() {
     return new Promise((resolve, reject) => {
-      fetch('http://api.icndb.com/jokes/random').then(res => res.json).then(data => {
+      fetch('http://api.icndb.com/jokes/random').then(res => res.json()).then(data => {
         resolve(data.value.joke)
       })
     })
